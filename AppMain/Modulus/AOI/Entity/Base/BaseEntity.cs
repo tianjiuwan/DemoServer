52057be2ef759e138079cf928a31e9c2740d5256
tianjiuwan <1007991486@qq.com>
2018-12-10 20:51:52 +0800

    1：服务器视野管理

---
+++ AppMain/Modulus/AOI/Entity/Base/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class BaseEntity
+{
+    //位置相关
+    public Vector3 position;
+    public int gridIndex;
+}
---
+++ AppMain/Modulus/AOI/Math/Vector3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// 2维向量
+/// </summary>
+public class Vector3
+{
+    public float x;
+    public float y;
+    public float z;
+
+    public Vector3(float x, float y, float z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    //距离
+    public static double distance(Vector3 from, Vector3 to)
+    {
+        float sx = from.x - to.x;
+        float sy = from.y - to.x;
+        float sz = from.z - to.z;
+        return Math.Sqrt(sx * sx + sy * sy + sz * sz);
+    }
+    //方向
+    public static Vector3 direction(Vector3 from, Vector3 to)
+    {
+        return new Vector3(to.x - from.x, to.y - from.y, to.z - from.z);
+    }
+
+}
+
